5da7cc7ea260679a75feab391e66fe50a2a52f33
Kang Min Ji <155235384+monshelle@users.noreply.github.com>
2025-09-09 20:31:03 +0900

    Update OwnerRepository.java

--- cardatabase/src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
@@ -1,7 +1,12 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
+
+@RepositoryRestResource
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
-	
+
+	Optional<Owner> findByFirstname(String firstname);
 }
