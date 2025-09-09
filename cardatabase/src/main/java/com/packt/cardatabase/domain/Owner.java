c9dab2bb2c6347f3a82e9c322a2421bf86b804ab
Kang Min Ji <155235384+monshelle@users.noreply.github.com>
2025-09-09 20:30:40 +0900

    Update Owner.java

--- cardatabase/src/main/java/com/packt/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -1,22 +1,16 @@
 package com.packt.cardatabase.domain;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.JoinColumn;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
@@ -34,18 +28,6 @@ public class Owner {
 		this.lastname=lastname;
 	}
 
-	@JsonIgnore
-	@OneToMany(cascade=CascadeType.ALL, mappedBy="owner")
-	private List<Car> cars;
-
-	public List<Car> getCars() {
-		return cars;
-	}
-
-	public void setCars(List<Car> cars) {
-		this.cars = cars;
-	}
-	
 	public long getOwnerid() {
 		return ownerid;
 	}
@@ -70,20 +52,17 @@ public class Owner {
 		this.lastname = lastname;
 	}
 	
-	
-	/*
-	@ManyToMany(cascade=CascadeType.PERSIST)
-	@JoinTable(name="car_owner",
-		joinColumns= {@JoinColumn(name="ownerid")},
-		inverseJoinColumns= {@JoinColumn(name="`id`")})
-	private Set<Car> cars = new HashSet<Car>();
+	@JsonIgnore
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="owner")
+	private List<Car> cars;
 
-	public Set<Car> getCars() {
+	public List<Car> getCars() {
 		return cars;
 	}
 
-	public void setCars(Set<Car> cars) {
+	public void setCars(List<Car> cars) {
 		this.cars = cars;
-	}*/
+	}
+	
 
 }
